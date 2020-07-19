e749fef3482b9f7ea5577af33289bdc060cbed0c
TLovell <trevorlovelldesign@gmail.com>
2020-07-19 10:40:01 -0600

    Added repetition in applying patterns

--- src/lib.rs
+++ src/lib.rs
@@ -15,29 +15,62 @@ struct Pattern {
     repeat: Repeat,
 }
 
+impl Repeat {
+    fn apply<F>(&self, mut op: F) -> io::Result<()>
+    where
+        F: FnMut() -> io::Result<bool>,
+    {
+        let mut repeat = true;
+        let mut rep_count: usize = 0;
+        while repeat {
+            repeat = op()?;
+            rep_count += 1;
+
+            if let Repeat::Finitely(x) = self {
+                if rep_count >= *x {
+                    repeat = false;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
 fn apply_pattern<T: io::BufRead>(
-    pattern: Pattern,
+    pattern: &Pattern,
     streams: &mut Vec<io::Lines<T>>,
 ) -> io::Result<Vec<String>> {
     let mut res: Vec<String> = vec![];
 
-    match pattern.data {
+    match &pattern.data {
         PatternData::Single(i) => {
-            if let Some(s) = streams.get_mut(i) {
-                if let Some(l) = s.next() {
-                    res.push(l?);
+            pattern.repeat.apply(|| -> io::Result<bool> {
+                if let Some(s) = streams.get_mut(*i) {
+                    if let Some(l) = s.next() {
+                        res.push(l?);
+                        Ok(true)
+                    } else {
+                        Ok(false)
+                    }
+                } else {
+                    Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("Pattern item out of file range: {}", i),
+                    ))
                 }
-            } else {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    format!("Pattern item out of file range: {}", i),
-                ));
-            }
+            })?;
         }
         PatternData::Group(g) => {
-            for inner_pattern in g {
-                res.extend(apply_pattern(inner_pattern, streams)?);
-            }
+            pattern.repeat.apply(|| -> io::Result<bool> {
+                let mut rep = false;
+                for inner_pattern in g {
+                    let inner_res = apply_pattern(&inner_pattern, streams)?;
+                    if inner_res.len() > 0 {
+                        rep = true;
+                    }
+                }
+                Ok(rep)
+            })?;
         }
     }
 
