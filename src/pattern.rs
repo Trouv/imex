a3ded3caf73646ce7e7a92a969ac975b0b0c924e
TLovell <trevorlovelldesign@gmail.com>
2020-07-29 01:12:06 -0600

    Wrote sad path parse tests

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,5 +1,5 @@
 use crate::repeater::Repeater;
-use std::io;
+use std::io::{BufRead, Error, ErrorKind::InvalidInput, Lines, Result};
 
 #[derive(PartialEq, Debug)]
 enum PatternData {
@@ -14,22 +14,19 @@ struct Pattern {
 }
 
 impl Pattern {
-    fn from(pattern: &str) -> io::Result<Pattern> {
+    fn from(pattern: &str) -> Result<Pattern> {
         Ok(Pattern {
             data: PatternData::Single(1),
             repeater: Repeater::Infinite,
         })
     }
 
-    fn merge_streams<T: io::BufRead>(
-        &self,
-        streams: &mut Vec<io::Lines<T>>,
-    ) -> io::Result<Vec<String>> {
+    fn merge_streams<T: BufRead>(&self, streams: &mut Vec<Lines<T>>) -> Result<Vec<String>> {
         let mut res: Vec<String> = vec![];
 
         match &self.data {
             PatternData::Single(i) => {
-                self.repeater.repeat(|| -> io::Result<bool> {
+                self.repeater.repeat(|| -> Result<bool> {
                     if let Some(s) = streams.get_mut(*i) {
                         if let Some(l) = s.next() {
                             res.push(l?);
@@ -38,15 +35,15 @@ impl Pattern {
                             Ok(false)
                         }
                     } else {
-                        Err(io::Error::new(
-                            io::ErrorKind::Other,
+                        Err(Error::new(
+                            InvalidInput,
                             format!("Pattern item out of file range: {}", i),
                         ))
                     }
                 })?;
             }
             PatternData::Group(g) => {
-                self.repeater.repeat(|| -> io::Result<bool> {
+                self.repeater.repeat(|| -> Result<bool> {
                     let mut rep = false;
                     for inner_pattern in g {
                         let inner_res = inner_pattern.merge_streams(streams)?;
@@ -68,7 +65,7 @@ mod parse_tests {
     use super::*;
 
     #[test]
-    fn empty_string_gives_empty_group_pattern() -> io::Result<()> {
+    fn empty_string_gives_empty_group_pattern() -> Result<()> {
         let p = Pattern::from("")?;
 
         assert_eq!(
@@ -82,7 +79,7 @@ mod parse_tests {
     }
 
     #[test]
-    fn repeats_gives_repeating_patterns() -> io::Result<()> {
+    fn repeats_gives_repeating_patterns() -> Result<()> {
         let p = Pattern::from("13{3}9*1")?;
 
         assert_eq!(
@@ -113,7 +110,7 @@ mod parse_tests {
     }
 
     #[test]
-    fn parens_gives_group_patterns() -> io::Result<()> {
+    fn parens_gives_group_patterns() -> Result<()> {
         let p = Pattern::from("1(1)(9)*(4){4}(1(1))()")?;
 
         assert_eq!(
@@ -173,33 +170,63 @@ mod parse_tests {
     }
 
     #[test]
-    fn bad_chars_fails() -> io::Result<()> {
-        Ok(())
+    fn bad_chars_fails() {
+        Pattern::from("0O0").unwrap_err();
+
+        Pattern::from("^[0]+$").unwrap_err();
+
+        Pattern::from("123*4{5}(x)*").unwrap_err();
     }
 
     #[test]
-    fn too_many_closed_parens_fails() -> io::Result<()> {
-        Ok(())
+    fn too_many_closed_parens_fails() {
+        Pattern::from("0(1)2)3(4)5").unwrap_err();
+
+        Pattern::from("0{1}2}3{4}5").unwrap_err();
+
+        Pattern::from("0(1)2(3))").unwrap_err();
     }
 
     #[test]
-    fn too_many_open_parens_fails() -> io::Result<()> {
-        Ok(())
+    fn too_many_open_parens_fails() {
+        Pattern::from("0(1)2(3(4)5").unwrap_err();
+
+        Pattern::from("0{1}2{3{4}5").unwrap_err();
+
+        Pattern::from("((0)1(2)3").unwrap_err();
     }
 
     #[test]
-    fn mismatched_parens_fails() -> io::Result<()> {
-        Ok(())
+    fn mismatched_parens_fails() {
+        Pattern::from(")(").unwrap_err();
+
+        Pattern::from("(3{)}").unwrap_err();
     }
 
     #[test]
-    fn bad_repeat_targets_fails() -> io::Result<()> {
-        Ok(())
+    fn bad_repeat_targets_fails() {
+        Pattern::from("(*4)").unwrap_err();
+
+        Pattern::from("({6}6)").unwrap_err();
+
+        Pattern::from("*2").unwrap_err();
+
+        Pattern::from("{4}4").unwrap_err();
+
+        Pattern::from("5{5}*").unwrap_err();
+
+        Pattern::from("5*{5}").unwrap_err();
+
+        Pattern::from("5{*5}").unwrap_err();
     }
 
     #[test]
-    fn bad_repeat_brace_contents_fails() -> io::Result<()> {
-        Ok(())
+    fn bad_repeat_brace_contents_fails() {
+        Pattern::from("5{5*}").unwrap_err();
+
+        Pattern::from("6{(6)}").unwrap_err();
+
+        Pattern::from("7{7{7}}").unwrap_err();
     }
 }
 
