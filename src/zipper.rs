6621e9016c1bc2b88898bc52045bc0ab8f47b7f5
TLovell <trevorlovelldesign@gmail.com>
2020-08-02 21:05:49 -0600

    Translated merge_tests for Pattern to tests for Zipper

--- src/zipper.rs
+++ src/zipper.rs
@@ -29,87 +29,95 @@ mod tests {
 
     #[test]
     fn non_repeating_zprex_might_not_complete() -> Result<()> {
-        let p = Zprex::from("01(10){3}")?;
-        let mut iters = vec!["0\n0\n0\n0\n0".lines(), "1\n1\n1\n1\n1".lines()];
+        let iters: Vec<Box<dyn Iterator<Item = char>>> =
+            vec![Box::from("00000".chars()), Box::from("11111".chars())];
+        let z = Zipper::from("01(10){3}", iters)?;
 
-        assert_eq!(
-            p.merge_iters(&mut iters)?,
-            vec!["0", "1", "1", "0", "1", "0", "1", "0"]
-        );
+        assert_eq!(z.map(|c| c.unwrap()).collect::<String>(), "01010");
 
         Ok(())
     }
 
     #[test]
     fn repeating_zprex_repeats() -> Result<()> {
-        let p = Zprex::from("0{3}1(01){5}")?;
-        let mut iters = vec!["0\n0\n0\n0\n0\n0\n0\n0".lines(), "1\n1\n1\n1\n1\n1".lines()];
+        let iters: Vec<Box<dyn Iterator<Item = char>>> =
+            vec![Box::from("00000000".chars()), Box::from("111111".chars())];
+        let z = Zipper::from("0{3}1(01){5}", iters)?;
 
-        assert_eq!(
-            p.merge_iters(&mut iters)?,
-            vec!["0", "0", "0", "1", "0", "1", "0", "1", "0", "1", "0", "1", "0", "1"]
-        );
+        assert_eq!(z.map(|c| c.unwrap()).collect::<String>(), "00010101010101");
 
         Ok(())
     }
 
     #[test]
     fn completed_zprex_exits_repeating() -> Result<()> {
-        let p = Zprex::from("0*(12)*")?;
-        let mut iters = vec![
-            "0\n0\n0".lines(),
-            "1\n1\n1".lines(),
-            "2\n2\n2\n2\n2".lines(),
+        let iters: Vec<Box<dyn Iterator<Item = char>>> = vec![
+            Box::from("000".chars()),
+            Box::from("111".chars()),
+            Box::from("22222".chars()),
         ];
+        let z = Zipper::from("0*(12)*", iters)?;
 
-        assert_eq!(
-            p.merge_iters(&mut iters)?,
-            vec!["0", "0", "0", "1", "2", "1", "2", "1", "2", "2", "2"]
-        );
+        assert_eq!(z.map(|c| c.unwrap()).collect::<String>(), "00012121222");
 
         Ok(())
     }
 
     #[test]
     fn out_of_range_zprex_fails() -> Result<()> {
-        let p = Zprex::from("0120")?;
-        let mut iters = vec!["0\n0\n0".lines(), "1\n1\n1".lines()];
+        let iters: Vec<Box<dyn Iterator<Item = char>>> =
+            vec![Box::from("000".chars()), Box::from("111".chars())];
+        let mut z = Zipper::from("0120", iters)?;
 
-        p.merge_iters(&mut iters).unwrap_err();
+        if let Some(r) = z.nth(2) {
+            r.unwrap_err();
+        } else {
+            panic!("Expected an error, not None");
+        }
 
         Ok(())
     }
 
     #[test]
     fn empty_zprex_gives_empty_merge() -> Result<()> {
-        let p = Zprex::from("")?;
-        let mut iters = vec!["0\n0\n0".lines(), "1\n1\n1".lines()];
+        let iters: Vec<Box<dyn Iterator<Item = char>>> =
+            vec![Box::from("000".chars()), Box::from("111".chars())];
+        let z = Zipper::from("", iters)?;
 
-        assert_eq!(p.merge_iters(&mut iters)?, Vec::<String>::new());
+        assert_eq!(z.map(|c| c.unwrap()).collect::<String>(), String::new());
 
         Ok(())
     }
 
     #[test]
     fn empty_iters_give_empty_merge() -> Result<()> {
-        let p = Zprex::from("0120")?;
-        let mut iters = vec!["".lines(), "".lines(), "".lines()];
+        let iters: Vec<Box<dyn Iterator<Item = char>>> = vec![
+            Box::from("".chars()),
+            Box::from("".chars()),
+            Box::from("".chars()),
+        ];
+        let z = Zipper::from("0120", iters)?;
 
-        assert_eq!(p.merge_iters(&mut iters)?, Vec::<String>::new());
+        assert_eq!(z.map(|c| c.unwrap()).collect::<String>(), String::new());
 
         Ok(())
     }
 
     #[test]
     fn empty_iter_list_only_passes_for_empty_zprex() -> Result<()> {
-        let p = Zprex::from("0120")?;
-        let mut iters: Vec<std::str::Lines> = vec![];
+        let iters: Vec<Box<dyn Iterator<Item = char>>> = vec![];
+        let mut z = Zipper::from("0120", iters)?;
 
-        p.merge_iters(&mut iters).unwrap_err();
+        if let Some(r) = z.nth(2) {
+            r.unwrap_err();
+        } else {
+            panic!("Expected an error, not None");
+        }
 
-        let p = Zprex::from("")?;
+        let iters: Vec<Box<dyn Iterator<Item = char>>> = vec![];
+        let z = Zipper::from("", iters)?;
 
-        assert_eq!(p.merge_iters(&mut iters)?, Vec::<String>::new());
+        assert_eq!(z.map(|c| c.unwrap()).collect::<String>(), String::new());
 
         Ok(())
     }
