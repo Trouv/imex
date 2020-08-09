33ce3a132b65430cca503c7c704ab9438891e23d
TLovell <trevorlovelldesign@gmail.com>
2020-08-08 22:58:15 -0600

    Removed AllZipsIntoOne and expanded on ZipsIntoOne.
    AllZipsIntoOne was sort of redundant with Zipper::from.

--- src/iter.rs
+++ src/iter.rs
@@ -1,40 +1,6 @@
 use crate::zipper::Zipper;
 use std::io::Result;
 
-trait AllZipsIntoOne<T, I>
-where
-    T: Iterator<Item = I>,
-{
-    fn zip_into_one_by_zprex(self, zprex: &str) -> Result<Zipper<T, I>>;
-
-    fn len(&self) -> usize;
-
-    fn zip_into_one(self) -> Zipper<T, I>
-    where
-        Self: Sized,
-    {
-        let iter_count = self.len() as u8;
-        let zprex = format!(
-            "({})*",
-            (0..iter_count).map(|x| x as char).collect::<String>()
-        );
-        self.zip_into_one_by_zprex(&zprex).unwrap()
-    }
-}
-
-impl<T, I> AllZipsIntoOne<T, I> for Vec<T>
-where
-    T: Iterator<Item = I>,
-{
-    fn zip_into_one_by_zprex(self, zprex: &str) -> Result<Zipper<T, I>> {
-        Zipper::<T, I>::from(self, zprex)
-    }
-
-    fn len(&self) -> usize {
-        Vec::len(self)
-    }
-}
-
 pub trait ZipsIntoOne<T, I>
 where
     T: Iterator<Item = I>,
@@ -43,14 +9,30 @@ where
 
     fn zip_into_one(self, iters: &mut Vec<T>) -> Zipper<T, I>
     where
-        Self: std::marker::Sized,
+        Self: Sized,
     {
         let iter_count = (iters.len() + 1) as u8;
         let zprex = format!(
             "({})*",
             (0..iter_count).map(|x| x as char).collect::<String>()
         );
-        self.zip_into_one_by_zprex(iters, &zprex).unwrap()
+        self.zip_into_one_by_zprex(iters, &zprex)
+            .expect("Default zprex should have been valid, but wasn't")
+    }
+
+    fn zip_both_into_one_by_zprex(self, other: T, zprex: &str) -> Result<Zipper<T, I>>
+    where
+        Self: Sized,
+    {
+        self.zip_into_one_by_zprex(&mut vec![other], zprex)
+    }
+
+    fn zip_both_into_one(self, other: T) -> Zipper<T, I>
+    where
+        Self: Sized,
+    {
+        self.zip_both_into_one_by_zprex(other, "(01)*")
+            .expect("Default zprex should have been valid, but wasn't")
     }
 }
 
